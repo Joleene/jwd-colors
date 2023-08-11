2109cd4ba2117205f504423b284b14e78f1eaa44
Joleene <joanna.lawinska1994@gmail.com>
2023-08-11 22:07:08 +0200

    Widok selektora kolorów

--- src/main/java/pl/edu/wszib/jwd/jwdcolors/controller/SelectedColorController.java
+++ src/main/java/pl/edu/wszib/jwd/jwdcolors/controller/SelectedColorController.java
@@ -1,14 +1,44 @@
-//package pl.edu.wszib.jwd.jwdcolors.controller;
-//
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.GetMapping;
-//
-//@Controller
-//public class SelectedColorController {
-//    @GetMapping("/select")
-//    public String selectedColorPage() {
-//        return "select";
-//
-//    }
-//
-//}
+package pl.edu.wszib.jwd.jwdcolors.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.thymeleaf.util.StringUtils;
+import pl.edu.wszib.jwd.jwdcolors.dao.SelectedColorDao;
+import pl.edu.wszib.jwd.jwdcolors.model.SelectedColor;
+
+import java.util.Date;
+
+@Controller
+public class SelectedColorController {
+
+    @Value("${app.title.select}")
+    private String title;
+
+@Autowired
+    SelectedColorDao selectedColorDao;
+    @GetMapping({"/select", "select/{color}"})
+    public String selectedColorPage(@PathVariable(required = false) String color, Model model) {
+
+        if(!StringUtils.isEmpty(color)) {
+            selectedColorDao.save(new SelectedColor(color, new Date()));
+
+        }
+
+        String[][] colors = {
+                {"red", "blue", "purple", "teal"},
+                {"black","orange", "yellow", "green"},
+                {"gray","silver", "olive", "lime"},
+                {"navy","white", "aqua", "fuchsia"}
+        };
+
+        model.addAttribute("tile", title);
+        model.addAttribute("colors", colors);
+        return "select";
+
+    }
+
+}
